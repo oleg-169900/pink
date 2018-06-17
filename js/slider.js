8d309c43e794740cc79e94028ffff61e665720a8
oleg-169900 <oleg-169900@yandex.ru>
2018-06-17 20:49:26 +0300

    написал стили для reviews

--- js/slider.js
+++ js/slider.js
@@ -1,16 +1,21 @@
 "use strict";
 
 (function () {
-    var reviewsList = document.querySelector('.reviews__list');
-    var noJS = document.querySelector('.reviews__inner');
+    var slider = document.querySelector('.reviews__list');
     var reviewsItem = document.querySelectorAll('.review');
-    var arrowLeft = document.querySelector('#arrow-left');
-    var arrowRight = document.querySelector('#arrow-right');
+    var arrowLeft = document.querySelector('#reviews-arrow-left');
+    var arrowRight = document.querySelector('#reviews-arrow-right');
     var width = 100;
     var position = 0;
     var count = 1;
+    var toggle1 = document.querySelector('#review-toggle-1');
+    var toggle2 = document.querySelector('#review-toggle-2');
+    var toggle3 = document.querySelector('#review-toggle-3');
 
-    noJS.classList.remove("reviews__inner--nojs");
+    if (slider.classList.contains('reviews__list--nojs')) {
+        slider.classList.remove('reviews__list--nojs');
+    }
+
 
     arrowLeft.onclick = function() {
       position = Math.min(position + width * count, 0)
@@ -20,5 +25,26 @@
     arrowRight.onclick = function() {
       position = Math.max(position - width * count, -width * (reviewsItem.length - count));
       reviewsList.style.marginLeft = position + '%';
+    };
+
+    toggle1.onclick = function() {
+      toggle2.classList.remove('reviews__toggle--active');
+      toggle3.classList.remove('reviews__toggle--active');
+      toggle1.classList.add('reviews__toggle--active');
+      slider.style.left = 0;
+    };
+
+    toggle2.onclick = function() {
+      toggle1.classList.remove('reviews__toggle--active');
+      toggle3.classList.remove('reviews__toggle--active');
+      toggle2.classList.add('reviews__toggle--active');
+      slider.style.left = '-100%';
+    };
+
+    toggle3.onclick = function() {
+      toggle1.classList.remove('reviews__toggle--active');
+      toggle2.classList.remove('reviews__toggle--active');
+      toggle3.classList.add('reviews__toggle--active');
+      slider.style.left = '-200%';
     };
 })();
